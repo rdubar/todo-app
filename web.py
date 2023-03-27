e2379b70f29b3b52bcd82980a621cfa1bd710036
Roger Dubar <rdubar@gmail.com>
2023-03-27 15:35:58 +0100

    Update web interface.

--- web.py
+++ web.py
@@ -5,28 +5,30 @@ import functions
 /home/rdubar/.p311/bin/streamlit run /home/rdubar/projects/todo-app/web.py
 """
 
+todos = functions.load_data()
+
 def add_todo():
     new = st.session_state['new_todo']
     print(new)
     if not new: return
-    data = functions.load_data()
-    while new in data:
+    while new in todos:
         new = new + ' '
-    data.append(new)
-    functions.save_data(data)
+    todos.append(new)
+    functions.save_data(todos)
     st.session_state["new_todo"] = ""
-
-
-todos = functions.load_data()
 
 
 st.title("My todo app")
 st.subheader("This is my todo app.")
 st.write('This app is to increase your productivity.')
 
-for todo in todos:
-    st.checkbox(todo)
-
+for index, todo in enumerate(todos):
+    checkbox = st.checkbox(todo, key=todo)
+    if checkbox:
+        todos.pop(index)
+        functions.save_data(todos)
+        del st.session_state[todo]
+        st.experimental_rerun()
 
 
 st.text_input(label="Input", placeholder="Add a new todo...", label_visibility='hidden',
